b80fcee5a32600ac35322848a8af27fdb04d1eb2
Irving Zhao <irvinezhao@gmail.com>
2018-05-25 11:08:37 +0800

    findUniq

---
+++ findUniq.js
@@ -0,0 +1,8 @@
+function findUniq(arr) {
+  // do magic
+  arr.sort((a,b)=>a-b);
+  return arr[0] === arr[1] ? arr[arr.length - 1] : arr[0]
+}
+console.log(findUniq([0,1,0]))
+console.log(findUniq([1,1,2,1,1,1]))
+console.log(findUniq([ 3, 10, 3, 3, 3 ]))
